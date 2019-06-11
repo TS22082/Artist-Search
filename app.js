b302272e6c98c3215c273b58ca3c961678d7303d
GeekCentric <ts22082@gmail.com>
2019-06-11 08:07:33 -0700

    clean up request string

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
   const API_KEY = '7e1bb37d8a2bdfa407f496ca3c1c8133'
-  const allowCors = 'https://cors-anywhere.herokuapp.com/'
+  const allowCors =
+    'https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/'
 
   $('#submitBtn').on('click', e => {
-    let artistQuery = $('#textInput').val()
-    const queryURL = `${allowCors}http://api.musixmatch.com/ws/1.1/artist.search?q_artist=${artistQuery}&page_size=5&apikey=${API_KEY}`
     e.preventDefault()
+    const artistQuery = $('#textInput').val()
+    const queryURL = `${allowCors}artist.search?q_artist=${artistQuery}&page_size=5&apikey=${API_KEY}`
     $.ajax({
       url: queryURL,
       method: 'GET'
     }).then(res => {
       res = JSON.parse(res)
-      console.log(res)
+      console.log(res.message.body.artist_list)
     })
   })
 })
