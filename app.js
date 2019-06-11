a053b3cec2cbd1ab48a1491498d085d51b57a411
GeekCentric <ts22082@gmail.com>
2019-06-11 06:57:31 -0700

    fixed cors error

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
-  const queryURL =
-    'https://musixmatchcom-musixmatch.p.rapidapi.com/wsr/1.1/artist.search?s_artist_rating=desc&q_artist=philthy rich&page=1&page_size=5'
+  const API_KEY = '7e1bb37d8a2bdfa407f496ca3c1c8133'
+  const allowCors = 'https://cors-anywhere.herokuapp.com/'
+  const queryURL = `${allowCors}http://api.musixmatch.com/ws/1.1/artist.search?q_artist=prodigy&page_size=5&apikey=${API_KEY}`
+
   $.ajax({
     url: queryURL,
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'musixmatchcom-musixmatch.p.rapidapi.com',
-      'X-RapidAPI-Key': 'b7436eb940msh7eda97d567495aap1562ccjsndf86c1bddee6'
-    }
+    method: 'GET'
   }).then(function(response) {
     response = JSON.parse(response)
     console.log(response)
