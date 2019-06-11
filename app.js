a5420c9f2d1f6e986b36dbc9f29d8abaecd62aef
GeekCentric <ts22082@gmail.com>
2019-06-11 08:02:40 -0700

    connect text input to api request

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   const API_KEY = '7e1bb37d8a2bdfa407f496ca3c1c8133'
   const allowCors = 'https://cors-anywhere.herokuapp.com/'
-  const queryURL = `${allowCors}http://api.musixmatch.com/ws/1.1/artist.search?q_artist=prodigy&page_size=5&apikey=${API_KEY}`
 
-  $.ajax({
-    url: queryURL,
-    method: 'GET'
-  }).then(function(response) {
-    response = JSON.parse(response)
-    console.log(response)
+  $('#submitBtn').on('click', e => {
+    let artistQuery = $('#textInput').val()
+    const queryURL = `${allowCors}http://api.musixmatch.com/ws/1.1/artist.search?q_artist=${artistQuery}&page_size=5&apikey=${API_KEY}`
+    e.preventDefault()
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).then(res => {
+      res = JSON.parse(res)
+      console.log(res)
+    })
   })
 })
