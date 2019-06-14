5fb13808976706cb9b1a29e311facfb8d7260017
GeekCentric <ts22082@gmail.com>
2019-06-14 11:36:52 -0700

    add related artist capablitity

--- app.js
+++ app.js
@@ -5,20 +5,49 @@ $(document).ready(function() {
 
   $('#submitBtn').on('click', e => {
     e.preventDefault()
+
     const artistQuery = $('#textInput').val()
     const queryURL = `${allowCors}artist.search?q_artist=${artistQuery}&page_size=5&apikey=${API_KEY}`
+
+    let artistID = ''
+    let artistList = ''
+    let relatedArtist = ''
+
+    $('#artistQueryList').empty()
+    $('#relatedArtist').empty()
+
     $.ajax({
       url: queryURL,
       method: 'GET'
-    }).then(res => {
-      res = JSON.parse(res)
-      const artistList = res.message.body.artist_list
-      console.log(artistList)
-      for (let i = 0; i < artistList.length; i++) {
-        $('#artistQueryList').append(
-          `<li class="list-group-item">${artistList[i].artist.artist_name}</li>`
-        )
-      }
     })
+      .then(res => {
+        res = JSON.parse(res)
+        artistList = res.message.body.artist_list
+        artistID = res.message.body.artist_list[0].artist.artist_id
+        for (let i = 0; i < artistList.length; i++) {
+          $('#artistQueryList').append(
+            `<li class="list-group-item">${
+              artistList[i].artist.artist_name
+            }</li>`
+          )
+        }
+      })
+      .then(() => {
+        const queryURL = `${allowCors}artist.related.get?artist_id=${artistID}&page_size=2&page=1&apikey=${API_KEY}`
+        $.ajax({
+          url: queryURL,
+          method: 'GET'
+        }).then(res => {
+          res = JSON.parse(res)
+          relatedArtist = res.message.body.artist_list
+          for (let i = 0; i < relatedArtist.length; i++) {
+            $('#relatedArtists').append(
+              `<li class="list-group-item">${
+                relatedArtist[i].artist.artist_name
+              }</li>`
+            )
+          }
+        })
+      })
   })
 })
